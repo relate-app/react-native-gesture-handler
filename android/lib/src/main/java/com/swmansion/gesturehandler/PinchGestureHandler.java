837a75d7336724e56ef313655836bdaca7962c6b
Tim Brandin <tim.brandin@gmail.com>
2021-11-19 13:54:49 +0100

    Fix for broken pinch gesture handler and eas build

--- android/lib/src/main/java/com/swmansion/gesturehandler/PinchGestureHandler.java
+++ android/lib/src/main/java/com/swmansion/gesturehandler/PinchGestureHandler.java
@@ -2,108 +2,131 @@ package com.swmansion.gesturehandler;
 
 import android.content.Context;
 import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.ViewConfiguration;
 
 public class PinchGestureHandler extends GestureHandler<PinchGestureHandler> {
-
-  private ScaleGestureDetector mScaleGestureDetector;
-  private double mLastScaleFactor;
-  private double mLastVelocity;
-
-  private float mStartingSpan;
-  private float mSpanSlop;
-
-  private ScaleGestureDetector.OnScaleGestureListener mGestureListener =
-          new ScaleGestureDetector.OnScaleGestureListener() {
-
-    @Override
-    public boolean onScale(ScaleGestureDetector detector) {
-      double prevScaleFactor = mLastScaleFactor;
-      mLastScaleFactor *= detector.getScaleFactor();
-      long delta = detector.getTimeDelta();
-      if (delta > 0) {
-        mLastVelocity = (mLastScaleFactor - prevScaleFactor) / delta;
-      }
-      if (Math.abs(mStartingSpan - detector.getCurrentSpan()) >= mSpanSlop
-              && getState() == STATE_BEGAN) {
-        activate();
-      }
-      return true;
-    }
-
-    @Override
-    public boolean onScaleBegin(ScaleGestureDetector detector) {
-      mStartingSpan = detector.getCurrentSpan();
-      return true;
-    }
-
-    @Override
-    public void onScaleEnd(ScaleGestureDetector detector) {
-      // ScaleGestureDetector thinks that when fingers are 27mm away that's a sufficiently good
-      // reason to trigger this method giving us no other choice but to ignore it completely.
-    }
-  };
+  private long time;
+  private double scale;
+  private double velocity;
+  private float startingSpan;
+  private float focalPointX, focalPointY;
 
   public PinchGestureHandler() {
-    setShouldCancelWhenOutside(false);
+  }
+
+  private float getX(MotionEvent event) {
+    float total = 0;
+    int i = 0;
+    for (; i < event.getPointerCount(); i++) {
+      try {
+        int id = event.getPointerId(i);
+        total += event.getX(id);
+      } catch (IllegalArgumentException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return total / i;
+  }
+
+  private float getY(MotionEvent event) {
+    float total = 0;
+    int i = 0;
+    for (; i < event.getPointerCount(); i++) {
+      try {
+        int id = event.getPointerId(i);
+        total += event.getY(id);
+      } catch (IllegalArgumentException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return total / i;
+  }
+
+  private float getSpan(MotionEvent event) {
+    float minX = Integer.MAX_VALUE;
+    float minY = Integer.MAX_VALUE;
+    float maxX = Integer.MIN_VALUE;
+    float maxY = Integer.MIN_VALUE;
+    for (int i = 0; i < event.getPointerCount(); i++) {
+      try {
+        int id = event.getPointerId(i);
+        float x = event.getX(id);
+        float y = event.getY(id);
+        minX = Math.min(minX, x);
+        maxX = Math.max(maxX, x);
+        minY = Math.min(minY, y);
+        maxY = Math.max(maxY, y);
+      } catch (IllegalArgumentException ex) {
+        ex.printStackTrace();
+      }
+    }
+    float spanX = maxX - minX;
+    float spanY = maxY - minY;
+    return (float)Math.sqrt(spanX * spanX + spanY * spanY);
   }
 
   @Override
   protected void onHandle(MotionEvent event) {
-    if (getState() == STATE_UNDETERMINED) {
-      Context context = getView().getContext();
-      mLastVelocity = 0f;
-      mLastScaleFactor = 1f;
-      mScaleGestureDetector = new ScaleGestureDetector(context, mGestureListener);
-      ViewConfiguration configuration = ViewConfiguration.get(context);
-      mSpanSlop = configuration.getScaledTouchSlop();
-
-      begin();
-    }
-
-    if (mScaleGestureDetector != null) {
-      mScaleGestureDetector.onTouchEvent(event);
-    }
-
+    int state = getState();
+    int action = event.getActionMasked();
     int activePointers = event.getPointerCount();
-    if (event.getActionMasked() == MotionEvent.ACTION_POINTER_UP) {
+    if (action == MotionEvent.ACTION_POINTER_UP) {
       activePointers -= 1;
     }
 
-    if (getState() == STATE_ACTIVE && activePointers < 2) {
+    if (activePointers >= 2) {
+      focalPointX = getX(event);
+      focalPointY = getY(event);
+      long prevTime = time;
+      time = event.getEventTime();
+
+      if (state == STATE_UNDETERMINED) {
+        velocity = 0.0;
+        scale = 1.0;
+        startingSpan = getSpan(event);
+        begin();
+      } else {
+        double prevScale = scale;
+        float span = getSpan(event);
+        
+        scale = (double)(span / startingSpan);
+        long delta = time - prevTime;
+        if (delta > 0) {
+          velocity = (scale - prevScale) / delta;
+        }
+
+        if (state == STATE_BEGAN) {
+          activate();
+        }
+      }
+    }
+
+    if (state == STATE_ACTIVE && activePointers < 2) {
       end();
-    } else if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+    } else if (action == MotionEvent.ACTION_UP) {
       fail();
     }
   }
 
   @Override
   protected void onReset() {
-    mScaleGestureDetector = null;
-    mLastVelocity = 0f;
-    mLastScaleFactor = 1f;
+    velocity = 0.0;
+    scale = 1.0;
   }
 
   public double getScale() {
-    return mLastScaleFactor;
+    return scale;
   }
 
   public double getVelocity() {
-    return mLastVelocity;
+    return velocity;
   }
 
   public float getFocalPointX() {
-    if (mScaleGestureDetector == null) {
-      return Float.NaN;
-    }
-    return mScaleGestureDetector.getFocusX();
+    return focalPointX;
   }
 
   public float getFocalPointY() {
-    if (mScaleGestureDetector == null) {
-      return Float.NaN;
-    }
-    return mScaleGestureDetector.getFocusY();
+    return focalPointY;
   }
 }
